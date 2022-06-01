d697fc34fcca57827683fb4ecb53861aaa01e38a
Xander Lubbe <x.lubbe@icloud.com>
2022-06-01 15:31:10 +0200

    Main up and running, along with graphDrawer

---
+++ graphDrawer.py
@@ -0,0 +1,31 @@
+from platform import node
+from node import Node
+from pyvis.network import Network
+import networkx as nx
+
+
+
+class GraphDrawer:
+    def __init__(self, nodes):
+        self.nodes = nodes
+        self.nt = Network('500px', '500px')
+
+    def generateNodes(self):
+        for item in self.nodes:
+            self.nt.add_node(item.nodeID, size=20, color=item.colour)
+
+    def generateEdges(self):
+        for node in self.nodes:
+            for edgeNode in node.edges:
+                self.nt.add_edge(node.nodeID, edgeNode.nodeID)
+    
+    def draw(self):
+        self.generateNodes()
+        self.generateEdges()
+        self.nt.toggle_physics(True)
+        self.nt.show('mygraph.html')
+
+
+
+
+    
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+from graphDrawer import GraphDrawer
 from node import Node
 from graph import Graph
 
 
-myNode1 = Node(1,[2,4,5],"none")
-myNode2 = Node(2,[1,3],"none")
-myNode3 = Node(3,[2],"none")
-myNode4 = Node(4,[1,5,6],"none")
-myNode5 = Node(5,[4,1],"none")
-myNode6 = Node(6,[4],"none")
+myNode1 = Node(1)
+myNode2 = Node(2)
+myNode3 = Node(3)
+myNode4 = Node(4)
+myNode5 = Node(5)
+myNode6 = Node(6)
+
+myNode1.edges = [myNode2,myNode4,myNode5]
+myNode2.edges = [myNode1,myNode3]
+myNode3.edges = [myNode2]
+myNode4.edges = [myNode1,myNode5,myNode6]
+myNode5.edges = [myNode4,myNode1]
+myNode6.edges = [myNode4]
+
 nodeArr = [myNode1, myNode2, myNode3, myNode4, myNode5, myNode6]
 
-
-
 myGraph = Graph(nodeArr)
-
 myGraph.graphColouring()
+
+drawGraph = GraphDrawer(nodeArr)
+drawGraph.draw()
+
